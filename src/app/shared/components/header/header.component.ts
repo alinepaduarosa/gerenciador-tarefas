1f6a9e13165888ad07683c3644cc846288807c31
Aline Pádua <aline.rosa@nexcore.com.br>
2025-03-03 15:23:43 -0300

    fix: remove componentes do Nx.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-header',
-    imports: [CommonModule],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss',
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {}
