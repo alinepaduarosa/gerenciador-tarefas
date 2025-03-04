2b159e0d1ff2ee38817e20dbdc68372b8b71dc87
Aline Pádua <aline.rosa@nexcore.com.br>
2025-03-04 12:24:08 -0300

    test(ListComponent): criando testes.

--- src/app/pages/list/list.component.spec.ts
+++ src/app/pages/list/list.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
+import { By } from '@angular/platform-browser';
 
 describe('ListComponent', () => {
-    let component: ListComponent;
     let fixture: ComponentFixture<ListComponent>;
 
     beforeEach(async () => {
@@ -11,11 +11,20 @@ describe('ListComponent', () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(ListComponent);
-        component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('deve listar as tarefas', () => {
+        const todoSection = fixture.debugElement.query(By.css('[data-testid="todo-list"]'));
+        expect(todoSection).toBeTruthy();
+
+        const todoItems = todoSection.queryAll(By.css('[data-testid="todo-list-item"]'));
+        expect(todoItems.length).toBe(3);
+
+        const completedSection = fixture.debugElement.query(By.css('[data-testid="completed-list"]'));
+        expect(completedSection).toBeTruthy();
+
+        const completedItems = completedSection.queryAll(By.css('[data-testid="completed-list-item"]'));
+        expect(completedItems.length).toBe(3);
     });
 });
